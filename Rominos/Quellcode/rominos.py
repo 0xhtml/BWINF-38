79766fdf4320df7f8379d2d24243abb6848c2668
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-09-07 20:13:11 +0200

    Kein Numpy

--- Rominos/Quellcode/rominos.py
+++ Rominos/Quellcode/rominos.py
@@ -1,53 +1,23 @@
 #!/usr/bin/python3
 import sys
 import itertools
-import numpy
 import PIL.Image
 import PIL.ImageDraw
-
-
-class ndenumerate(numpy.ndenumerate):
-    def __init__(self, arr):
-        # Vereinfacht von
-        # https://github.com/numpy/numpy/blob/c23bd98de665733dd090636f38b6aacf9f99450b/numpy/lib/index_tricks.py#L582-L583
-        self.iter = arr.flat
-
-
-def rot90(m, k=1):
-    # Vereinfacht von
-    # https://github.com/numpy/numpy/blob/17ff82a7f3ca49717128b89d1a5560f4545fda0f/numpy/lib/function_base.py#L73-L156
-    if k == 0:
-        return m[:]
-    elif k == 1:
-        return numpy.transpose(flip(m, 1), (1, 0))
-    elif k == 2:
-        return flip(flip(m, 0), 1)
-    else:
-        # k == 3
-        return flip(numpy.transpose(m, (1, 0)), 1)
-
-
-def flip(m, axes=0):
-    # Vereinfacht von
-    # https://github.com/numpy/numpy/blob/17ff82a7f3ca49717128b89d1a5560f4545fda0f/numpy/lib/function_base.py#L164-L254
-    indexer = [slice(None)] * m.ndim
-    indexer[axes] = slice(None, None, -1)
-    indexer = tuple(indexer)
-    return m[indexer]
+import time
 
 
 class Romino:
     """Ein einzelner Romino bestehend aus n Quadraten"""
 
-    def __init__(self, array):
+    def __init__(self, quadrate):
         # Speichere die ursprüngliche Größe
-        self.größe = len(array)
+        self.größe = len(quadrate)
 
         # Speichere ob dieser Romino bereits verschoben wurde
         self.verschoben = False
 
         # Speichere die übergebenen Quadrate
-        self.array = array
+        self.quadrate = quadrate
 
         # Erstelle Dict für desckungsgleiche Rominos
         self.deckungsgleiche = {}
@@ -70,27 +40,17 @@ class Romino:
         V
         """
         if not self.verschoben:
-            # Ermittle die mini- und maximalen X- und Y-Position
-            min_x = array.shape[0]
-            min_y = array.shape[1]
-            max_x = 0
-            max_y = 0
-            for x in range(array.shape[0]):
-                for y in range(array.shape[1]):
-                    if array[x][y] == 0:
-                        continue
+            # Ermittle die minimalen X- und Y-Position
+            x = self.größe
+            y = self.größe
+            for quadrat in self.quadrate:
+                if quadrat[0] < x:
+                    x = quadrat[0]
+                if quadrat[1] < y:
+                    y = quadrat[1]
 
-                    if x < min_x:
-                        min_x = x
-                    if y < min_y:
-                        min_y = y
-                    if x > max_x:
-                        max_x = x
-                    if y > max_y:
-                        max_y = y
-
-            # Entferne alles bis and die mini- und maximalen Positionen
-            self.array = array[min_x:max_x + 1, min_y:max_y + 1]
+            # Verschiebe die Quadrate
+            self.quadrate = set(map(lambda q: (q[0] - x, q[1] - y), self.quadrate))
             self.verschoben = True
 
     def gültig(self):
@@ -102,42 +62,32 @@ class Romino:
         nur mit der Ecke (also diagonal) berühren.
         """
         # Teste ob alle Quadrate mit einander verbunden sind
-        def suche(position, gefunden=set()):
+        def verbunden(position, gefunden):
             x, y = position
             o = {(-1, -1), (-1, 0), (-1, 1), (0, -1),
                  (0, 1), (1, -1), (1, 0), (1, 1)}
             for xo, yo in o:
                 if len(gefunden) == self.größe:
                     break
-                if (x + xo) < 0 or \
-                        (y + yo) < 0 or \
-                        (x + xo) >= self.array.shape[0] or \
-                        (y + yo) >= self.array.shape[1]:
-                    continue
                 position = (x + xo, y + yo)
-                if self.array[x + xo][y + yo] == 1 and position not in gefunden:
+                if position in self.quadrate and position not in gefunden:
                     gefunden.add(position)
-                    suche(position, gefunden)
-            return gefunden
-
-        # Suche nach dem ersten Quadrat
-        for position, wert in ndenumerate(self.array):
-            # Beende bei der ersten gefundenen Position
-            if wert == 1:
-                break
+                    verbunden(position, gefunden)
+            return len(gefunden)
 
         # Suche nach allen verbundenen Quadraten
-        verbundene_quadrate = suche(position, {position})
+        erstes_quadrat = self.quadrate.__iter__().__next__()
+        verbundene_quadrate = verbunden(erstes_quadrat, {erstes_quadrat})
 
         # Teste ob alle Quadrate gefunden wurden
-        if len(verbundene_quadrate) != self.größe:
+        if verbundene_quadrate != self.größe:
             # Es wurden nicht alle Quadrate gefunde. Das heißt, dass nicht
             # alle Quadrate verbuden sind.
             return False
 
         # Teste ob mindestens eine diagonale Verbindung vorhanden ist:
         # Gehe durch alle Quadrate
-        for (x, y) in verbundene_quadrate:
+        for (x, y) in self.quadrate:
             # Alle diagonalen Positionen
             o = {(-1, -1), (-1, 1), (1, -1), (1, 1)}
 
@@ -145,18 +95,26 @@ class Romino:
             for xo, yo in o:
                 # Teste ob die diagonale Vorhanden ist und keine gerade
                 # Verbindung vorhanden ist
-                if (x + xo, y + yo) in verbundene_quadrate and \
-                    (x + xo, y) not in verbundene_quadrate and \
-                        (x, y + yo) not in verbundene_quadrate:
+                if (x + xo, y + yo) in self.quadrate and \
+                    (x + xo, y) not in self.quadrate and \
+                        (x, y + yo) not in self.quadrate:
                     # Es ist eine diagonale Verbindung vorhanden
                     return True
 
         # Es ist keine diagonale Verbindung vorhanden
         return False
 
-    def gleich(self, array):
-        """Diese Funktion testet ob zwei Rominos gleich sind."""
-        return self.array.shape == array.shape and (self.array == array).all()
+    def spiegeln(self, quadrate, achse):
+        max = 0
+        for quadrat in quadrate:
+            if quadrat[achse] > max:
+                max = quadrat[achse]
+
+        funktion = lambda q: q[:achse] + (max - q[achse],) + q[achse + 1:]
+        return set(map(funktion, quadrate))
+
+    def achsen_tauschen(self, quadrate):
+        return set(map(lambda q: q[::-1], quadrate))
 
     def deckungsgleich(self, romino):
         """
@@ -167,50 +125,23 @@ class Romino:
         self.verschieben()
         romino.verschieben()
 
-        # Teste ob das Format (Seitenlängen) der beiden Rominos gleich ist
-        if self.array.shape == romino.array.shape and \
-                self.array.shape == romino.array.shape[::-1]:
-            rotationen = {0, 1, 2, 3}
-        elif self.array.shape == romino.array.shape:
-            rotationen = {0, 2}
-        elif self.array.shape == romino.array.shape[::-1]:
-            rotationen = {1, 3}
-        else:
-            return False
-
-        # Lade bereits gespeicherte rotiert/gepsiegelte Rominos
-        gespeichert = self.deckungsgleiche.keys()
-
-        # Gehe durch alle Rotationen
-        for rotation in rotationen:
-            # Rotiere den Romino
-            if (rotation, False) in gespeichert:
-                array = self.deckungsgleiche[(rotation, False)]
+        axen_tauschen_bools = [False, True]
+        spiegeln_bools = [(False, False), (True, False),
+                          (False, True), (True, True)]
+        for axen_tauschen_bool in axen_tauschen_bools:
+            if axen_tauschen_bool:
+                achsen_getauscht = self.achsen_tauschen(self.quadrate)
             else:
-                array = rot90(self.array, rotation)
-                if rotation != 0:
-                    # Speichere den rotierten Romino ab
-                    self.deckungsgleiche[(rotation, False)] = array
+                achsen_getauscht = self.quadrate
 
-            # Gehe durch alle Spiegelungen
-            for spiegeln in (True, False):
-                # Teste ob der Romino gespiegelt werden soll
-                if spiegeln:
-                    if (rotation, True) in gespeichert:
-                        gespiegeltes_array = self.deckungsgleiche[(
-                            rotation, True)]
-                    else:
-                        # Spiegel den Romino
-                        gespiegeltes_array = flip(array)
+            for spiegeln_bool in spiegeln_bools:
+                gespiegelt = achsen_getauscht
+                if spiegeln_bool[0]:
+                    gespiegelt = self.spiegeln(gespiegelt, 0)
+                if spiegeln_bool[1]:
+                    gespiegelt = self.spiegeln(gespiegelt, 1)
 
-                        # Speichere den gespiegelten Romino ab
-                        self.deckungsgleiche[(
-                            rotation, True)] = gespiegeltes_array
-                else:
-                    gespiegeltes_array = array
-
-                # Vergleiche die beiden Rominos
-                if romino.gleich(gespiegeltes_array):
+                if romino.quadrate == gespiegelt:
                     return True
         return False
 
@@ -220,18 +151,18 @@ class Romino:
             None,
             (100, 100, 100)
         )
-        for x2 in range(self.array.shape[0]):
-            for y2 in range(self.array.shape[1]):
-                if self.array[x2][y2] == 1:
-                    draw.rectangle(
-                        [(x2 * 10 + x, y2 * 10 + y),
-                         (x2 * 10 + 10 + x, y2 * 10 + 10 + y)],
-                        (0xcf, 0x2b, 0x1d),
-                        (0, 0, 0)
-                    )
+        for quadrat in self.quadrate:
+            draw.rectangle(
+                [(quadrat[0] * 10 + x, quadrat[1] * 10 + y),
+                    (quadrat[0] * 10 + 10 + x, quadrat[1] * 10 + 10 + y)],
+                (0xcf, 0x2b, 0x1d),
+                (0, 0, 0)
+            )
 
 
 if __name__ == "__main__":
+    start = time.time()
+
     if len(sys.argv) != 3:
         # Es wurden nicht alle Argumente angegeben
         print("Benutzung:", sys.argv[0], "n ausgabedateiname")
@@ -256,15 +187,8 @@ if __name__ == "__main__":
     # Teste alle möglichen Rominos auf Gültigkeit
     gültige_rominos = set()
     for romino_quadrate in mögliche_rominos:
-        # Erstelle leeren Romino
-        array = numpy.zeros((n, n))
-
-        # Füge Quadrate zu Romino hinzu
-        for quadrat in romino_quadrate:
-            array[quadrat] = 1
-
         # Erstelle Romino
-        romino = Romino(array)
+        romino = Romino(set(romino_quadrate))
 
         # Teste Gültigkeit des Romino
         if romino.gültig():
@@ -292,3 +216,5 @@ if __name__ == "__main__":
             y += 1
     draw.text((1, bild.height-10), str(len(gültige_rominos)), (0, 0, 0))
     bild.save(sys.argv[2])
+
+    print(time.time() - start)
