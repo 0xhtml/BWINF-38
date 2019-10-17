95ca60b7c7a706ff1bb954b83dfdc8a8435a9e23
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-10-17 14:31:32 +0200

    Document first two functions of rominos

--- Rominos/Quellcode/rominos2.py
+++ Rominos/Quellcode/rominos2.py
@@ -1,32 +1,68 @@
 import sys
-import time
 import math
 
 
-def nachbarn(q, nachbarn):
-    nachbarn = {(x + q[0], y + q[1]) for x in nachbarn for y in nachbarn}
+def nachbarn(quadrat, gerade=True):
+    """Diese Funktion berechnet die Nachbarn eines Quadrates"""
+
+    # Setzte Richtung (x- o. y-Verschiebung) jenachdem, ob gerade Nachbarn
+    # generiert werden sollen
+    if gerade:
+        richtung = {-1, 0, 1}
+    else:
+        richtung = {-1, 1}
+
+    # Generiere alle Nachbarn mit Richtung
+    nachbarn = {
+        (x + quadrat[0], y + quadrat[1])
+        for x in richtung
+        for y in richtung
+    }
+
+    # Filtere alle Nachbarn raus, die im negativem bereich sind.
     nachbarn = filter(lambda x: x[0] >= 0 and x[1] >= 0, nachbarn)
+
+    # Gebe die Nachbarn zurück
     return nachbarn
 
 
-def nächste(romino_anfang, n):
-    if len(romino_anfang) == 1:
-        nächste = {y for x in romino_anfang for y in nachbarn(x, {-1, 1})}
-    else:
-        nächste = {y for x in romino_anfang for y in nachbarn(x, {-1, 0, 1})}
-        nächste.remove((romino_anfang[0][0], romino_anfang[1][1]))
-        nächste.remove((romino_anfang[1][0], romino_anfang[0][1]))
+def nächste(romino, größe):
+    """Diese Funktion generiert die nächsten Quadrate für einen Romino"""
 
-    if len(romino_anfang) == n - 1:
-        null = set(filter(lambda x: x[1] == 0, romino_anfang))
+    # Teste, ob dies das erstemal Generieren der Quadrate ist
+    if len(romino) == 1:
+        # Generiere nur diagonale Nachbarn
+        nächste = {y for x in romino for y in nachbarn(x, False)}
+    else:
+        # Generiere diagonale un gerade Nachbarn
+        nächste = {y for x in romino for y in nachbarn(x)}
+
+        # Entferne gerade Nachbarn neben der ersten diagonalen Verbindung, damit
+        # diese erhalten bleibt
+        nächste.remove((romino[0][0], romino[1][1]))
+        nächste.remove((romino[1][0], romino[0][1]))
+
+    # Teste, ob dies das letze mal Generieren der Quadrate ist
+    if len(romino) == größe - 1:
+        # Finde alle Quadrate, für die y=0 gilt
+        null = set(filter(lambda x: x[1] == 0, romino))
+
+        # Teste, ob für keine Quadrate y=0 gilt
         if len(null) == 0:
+            # Wenn für keine Quadrate y=0 gilt, muss im beim letzten ma
+            # Generieren der Quadrate y=0 gelten
+            # Filtere alle Quadrate raus, bei denen nicht y=0 gilt
             nächste = filter(lambda x: x[1] == 0, nächste)
 
-    nächste = filter(lambda x: x not in romino_anfang, nächste)
+    # Filtere alle Quadrate raus, die bereits im Romino vorhanden sind
+    nächste = filter(lambda x: x not in romino, nächste)
+
+    # Gib die generierten Quadrate zurück
     return nächste
 
 
 def gefunden(romino, rominos):
+    """Diese Funktion testet, ob ein romino bereits gefunden wurde"""
     spiegelungen = [(x, y, z) for x in range(2)
                     for y in range(2) for z in range(2)]
     for spiegelung in spiegelungen:
@@ -55,35 +91,36 @@ if __name__ == "__main__":
     if len(sys.argv) != 3 and len(sys.argv) != 2:
         print("Nutzung:", sys.argv[0], "n ausgabedatei")
         print("   oder:", sys.argv[0], "n")
-        exit()
+        sys.exit()
 
-    n = int(sys.argv[1])
+    größe = int(sys.argv[1])
     datei = sys.argv[2] if len(sys.argv) == 3 else None
 
-    print(f"Starte berechnung für n={n}")
+    print(f"Starte berechnung für n={größe}")
 
-    rominos = [[(0, y)] for y in range(math.floor(n / 2))]
+    rominos = [[(0, y)] for y in range(math.floor(größe / 2))]
 
-    for _ in range(n - 1):
-        rominos = [x + [y] for x in rominos for y in nächste(x, n)]
+    for _ in range(größe - 1):
+        rominos = [x + [y] for x in rominos for y in nächste(x, größe)]
 
     rominos = {tuple(sorted(x)) for x in rominos}
 
-    gefilterte_rominos = []
+    gefilterte_rominos = set()
     for romino in rominos:
         if not gefunden(romino, gefilterte_rominos):
-            gefilterte_rominos.append(romino)
+            gefilterte_rominos.add(romino)
 
     print(f"Es wurden {len(gefilterte_rominos)} Rominos gefunden.")
-    if datei != None:
+    if datei is not None:
         datei = datei.split(".")
         datei = ".".join(datei[:-1]) if len(datei) > 1 else datei[0]
         print(f"Speichere Rominos in {datei}.txt ab.")
         datei = open(datei + ".txt", "w")
-        datei.write(f"{len(gefilterte_rominos)} Rominos der Größe n={n}\n\n")
+        datei.write(
+            f"{len(gefilterte_rominos)} Rominos der Größe n={größe}\n\n")
         for romino in gefilterte_rominos:
-            for x in range(n):
-                for y in range(n):
+            for x in range(größe):
+                for y in range(größe):
                     if (x, y) in romino:
                         datei.write("█")
                     else:
